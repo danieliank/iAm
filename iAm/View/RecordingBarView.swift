990f4b89eccc1f410d1860b359e845a70431a8ac
ravi <alessandrowirawan@gmail.com>
2024-07-11 20:12:05 +0700

    merge toolbar n navbar to one file

---
+++ iAm/View/RecordingBarView.swift
@@ -0,0 +1,28 @@
+//
+//  RecordingBarView.swift
+//  iAm
+//
+//  Created by Raphael on 11/07/24.
+//
+
+import SwiftUI
+
+struct RecordingBarView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                .stroke()
+                .frame(width: 353, height: 95)
+                .foregroundStyle(.mint)
+            Image(systemName: "play.fill")
+                .resizable()
+                .frame(width: 30, height: 35)
+                .foregroundStyle(.mint)
+        }
+        
+    }
+}
+
+#Preview {
+    RecordingBarView()
+}
