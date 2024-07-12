cfc9bf7275145d724d3cef1005ef499f776a4948
danieliank <daxter1835@gmail.com>
2024-07-12 11:06:07 +0700

    add enum for prompt

--- iAm/Helper/Enums.swift
+++ iAm/Helper/Enums.swift
@@ -60,7 +60,20 @@ enum Mood: Codable, CaseIterable {
         }
     }
     
-    // TODO: add prompt
+    var prompt: [String] {
+        switch self {
+        case .unpleasant:
+            return ["What made you feel this way?", "How long have you felt unpleasant?", "Is there something that might help improve your mood?"]
+        case .slightlyUnpleasant:
+            return ["What's causing this slight discomfort?", "Is there anything that could alleviate this feeling?", "How can you make today better?"]
+        case .neutral:
+            return ["What's on your mind?", "Do you feel balanced today?", "Is there something that could make your day better?"]
+        case .slightlyPleasant:
+            return ["What's making you feel good?", "Can you pinpoint the cause of this pleasant feeling?", "How can you maintain this positivity?"]
+        case .pleasant:
+            return ["What made you very happy today?", "Can you share what's bringing you joy?", "How can you carry this positive energy forward?"]
+        }
+    }
 }
 
 enum Destination: Hashable {
