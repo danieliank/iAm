049becb641147fab442fe1879e8500cb2428a7cb
alfadlimaulana <alfadlimaulana@gmail.com>
2024-07-15 20:48:47 +0700

    feat: new HistoryView and stateOfMind View
    * stateOfMind is now a sheet
    * note list cards is now updated

--- iAm/View/ContentView.swift
+++ iAm/View/ContentView.swift
@@ -9,60 +9,34 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var context
-    
-    @State var moodValue: Mood = .neutral
     @StateObject var navPath = Router.shared
+    @State var showSheet = false
     
     var body: some View {
         NavigationStack(path: $navPath.path) {
-            ScrollView(.vertical) {
-                VStack {
-                    Text("How do you feel right now?")
-                        .font(.headline)
-                    TabView(selection: $moodValue) {
-                        ForEach(Mood.allCases, id: \.self) { mood in
-                            VStack {
-                                Image(mood.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 280)
-                                    .padding([.top,.bottom], 40)
-                                    
-                                Text(mood.title)
-                                .font(.system(size: 30, weight: .bold))}
-                        }
-                    } .frame(width: .infinity, height: 450)
-                    
-                        .tabViewStyle(.page(indexDisplayMode: .never))
-                    
-                    Button {
-                        let newNote = Note(mood: moodValue, content: "", timestamp: Date())
-                        context.insert(newNote)
-                        
-                        Router.shared.path.append(.noteView(note: newNote))
-                    } label: {
-                        Text("Log Emotion")
-                            .frame(width: 115, height: 18)
-                            .padding()
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(12)
-                    }
-                    .padding(.top, 50)
-                }
-                .frame(height: UIScreen.main.bounds.height)
-                
-                HistoryView()
-            }
+            HistoryView()
+                .padding(.top, 24)
             .navigationDestination(for: Destination.self) { destination in
                 switch destination {
                 case .noteView(let note):
                     NoteView(note: note)
                 }
             }
-            .ignoresSafeArea()
+            .navigationTitle("iAm")
+            .toolbar {
+                ToolbarItemGroup {
+                    Button("Add", systemImage: "plus.circle.fill") {
+                        showSheet = true
+                    }
+                }
+            }
+            .sheet(isPresented: $showSheet, content: {
+                StateOfMindView(showSheet: $showSheet)
+                    .presentationDragIndicator(.visible)
+                    .presentationCornerRadius(10)
+            })
         }
+        .ignoresSafeArea(.all)
     }
 }
 
